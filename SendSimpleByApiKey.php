4f69fc09ebbde704cbc9e6dc625e33ebb4439775
bgsrb88 <bgsrb88@gmail.com>
2016-10-09 12:28:42 +0330

    add SendSimpleByApiKey

--- SendSimpleByApiKey.php
+++ SendSimpleByApiKey.php
@@ -1,30 +1,35 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 ini_set("soap.wsdl_cache_enabled", "0");
-try{
-	//$client= new SoapClient('https://api.kavenegar.com:443/soap/v1.asmx?WSDL',array('trace' => 1));
-	$client= new SoapClient('http://api.kavenegar.com/soap/v1.asmx?WSDL',array('trace' => 1));
-	$apikey = "";
-	$sender = "";
-	$message = "";
-	$receptor = array("");
-	$params = array(
-				'apikey'   		=> $apikey,
-				'sender'   		=> $sender,
-				'message'  		=> $message,
-				'receptor' 		=> $receptor,
-				'unixdate' 		=> 0,
-				'msgmode'  		=> 1,
-				'status'   		=> "",
-				'statusmessage' => ""
-			);
-	$result=$client->SendSimpleByApikey($params);	
-	if($result->status==200){
-		var_dump($result->SendSimpleByApikeyResult->long);
-	}else{
-		echo "status = $result->status & StatusMessage = $result->statusmessage";
-	}
-}catch (SoapFault $fault) {
+try {
+    //$client= new SoapClient('https://api.kavenegar.com:443/soap/v1.asmx?WSDL',array('trace' => 1));
+    $client   = new SoapClient('http://api.kavenegar.com/soap/v1.asmx?WSDL', array(
+        'trace' => 1
+    ));
+    $apikey   = "";
+    $sender   = "";
+    $message  = "";
+    $receptor = array(
+        ""
+    );
+    $params   = array(
+        'apikey' => $apikey,
+        'sender' => $sender,
+        'message' => $message,
+        'receptor' => $receptor,
+        'unixdate' => 0,
+        'msgmode' => 1,
+        'status' => "",
+        'statusmessage' => ""
+    );
+    $result   = $client->SendSimpleByApikey($params);
+    if ($result->status == 200) {
+        var_dump($result->SendSimpleByApikeyResult->long);
+    } else {
+        echo "status = $result->status & StatusMessage = $result->statusmessage";
+    }
+}
+catch (SoapFault $fault) {
     trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
 }
 ?>
